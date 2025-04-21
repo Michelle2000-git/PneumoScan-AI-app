2d0e9957a1fc3c49ae1816477ae6aaf077ec02cb
Michelle2000-git <sandinhamichelle9@gmail.com>
2025-04-21 20:08:47 +0200

    Create PneumoScan AI Streamlit_app.py

---
+++ PneumoScan AI Streamlit_app.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+from PIL import Image
+import base64
+
+#Loading the trained CNN model
+MODEL_PATH = "/kaggle/input/mymodel/my_model2.hdf5"
+model = load_model(MODEL_PATH)
+
+#Class names (index 0 = NORMAL, index 1 = PNEUMONIA)
+class_names = ['NORMAL', 'PNEUMONIA']
+
+#Streamlit page setup
+st.set_page_config(page_title="Pneumonia Detection", layout="centered")
+st.title("""
+         Welcome to PneumoScan AI
+         Your AI-Assisted X-ray Review for Paediatric Pneumonia
+         """
+         )
+uploaded_file = st.file_uploader("Please upload a chest x-ray image...", type=["jpg", "jpeg", "png"])
+
+if uploaded_file is not None:
+    #Display uploaded image
+    img = Image.open(uploaded_file).convert("RGB")
+    st.image(img, caption="Uploaded X-ray", use_container_width=True)
+
+    #Preprocess the image
+    img = img.resize((224, 224))
+    img_array = image.img_to_array(img)
+    img_array = np.expand_dims(img_array, axis=0)
+    img_array = img_array / 255.0  
+
+    # Make prediction
+    prediction = model.predict(img_array)
+    pneumonia_confidence = float(prediction[0][1])
+
+    #Applying threshold logic
+    threshold = 0.685  
+    if pneumonia_confidence > threshold:
+        predicted_class = "PNEUMONIA"
+        confidence = pneumonia_confidence
+    else:
+        predicted_class = "NORMAL"
+        confidence = 1 - pneumonia_confidence
+
+    #Display prediction results
+    st.markdown("---")
+    st.subheader("Prediction Result")
+    st.write(f"This image most likely represents a {predicted_class} chest x-ray")
